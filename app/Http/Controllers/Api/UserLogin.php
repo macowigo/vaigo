9dab0b3e8d2de06aa50402de8d7fbdb0104015db
macowigo <owigosabbath@gmail.com>
2022-08-22 15:27:57 +0300

    user loginapi

--- app/Http/Controllers/Api/UserLogin.php
+++ app/Http/Controllers/Api/UserLogin.php
@@ -3,17 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserLogin extends Controller
 {
-    function login(Request $request){
-        $email=$request->email;
-        $password=$request->password;
-        if (Auth::attempt(['email' => $email, 'password' => $password])) {
-            return ['attemp'=>'good'];
+    public function login(Request $request){
+        $phone = $request->phone;
+        $password = $request->password;
+        $user = User::WHERE('phone', $request->phone)->first();
+        if ($user){
+        if(Hash::check($request->password, $user->password))
+        {
+             $token = $user->createToken("MyToken")->plainTextToken;
+             $response = ['status' => true, 'token' => $token, 'user' => $user, 'message' => 'login successful'];
         }
-        return['attemp'=>'invalid credentials'];
+        else{
+            $response = ['status' => false, 'message' =>'incorrect password'];
+        } 
+    }
+    else{
+        $response = ['status' => false, 'message' => 'incorrect phone number'];
+    }
+            return $response;
+        
     }
 }
